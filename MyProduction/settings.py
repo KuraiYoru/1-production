d66a68b8f0d537a9beccd8b31e6525add371cd2f
Mark <91343411+KuraiYoru@users.noreply.github.com>
2022-09-05 19:47:00 +0300

    ver 0.0.1

--- MyProduction/settings.py
+++ MyProduction/settings.py
@@ -124,4 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 STATIC_ROOT = os.path.join(BASE_DIR, '../example/static')
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
